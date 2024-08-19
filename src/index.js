7ca441c794a7c475ec0fd511a382d60ba6a6b997
Lev Staver <levstaver@yandex.ru>
2024-08-20 01:12:44 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketsPair = {};
+  let stack = [];
+
+  bracketsConfig.forEach(
+    (brackets) => (bracketsPair[brackets[0]] = brackets[1])
+  );
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i];
+    bracketsPair[stack[stack.length - 1]] == current
+      ? stack.pop()
+      : stack.push(current);
+  }
+  return stack.length === 0;
+};
